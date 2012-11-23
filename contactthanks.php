d7a543175a3d0d8eb96c341e0ba5621cd9a6c587
Ｄａｖｉｄ Ｈｉｇｇｉｎｓ <manicstrike@gmail.com>
2012-11-23 18:18:19 +0000

    The one true commit

---
+++ contactthanks.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Thanks!</title>
+    <style>
+    @font-face{
+font-family: "karla";
+src: url("karla.ttf") format("truetype");
+}
+
+*{
+font-family: 'karla'
+}
+
+</style>
+</head>
+
+<body>
+<center>
+<h1>Thanks. I will get back to you shortly. Panda approves 🐼</h1><br>
+<h3><a href="form.html">&larr;Send another message</a></h3>
+</center>
+</body>
+</html>
